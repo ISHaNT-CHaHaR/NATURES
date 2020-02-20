519e393bbf49cd0ede0b63442dc4445d2cc5599e
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-02-20 18:53:47 +0530

    added MIddleware in express
    .

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
+const fs = require('fs');
 const express = require('express');
+
 const app = express();
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Herro from server', app: 'Natours' });
+app.use(express.json());
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Herro from server', app: 'Natours' });
+// });
+// app.post('/',(req,res)=>{
+//     res.send(`You can post to this input.... `);
+// })
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+
+    status: 'success',
+    results: tours.length ,
+    data: {
+      tours
+    }
+  });
 });
-app.post('/',(req,res)=>{
-    res.send(`You can post to this input.... `); 
-})
 
-
-
-const port = 3000; 
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`APP runnning on port ${port}.....`);
 });
-
-

ca0d59879eb62da1fc9fc4267d4112da84885071
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-02-21 14:54:57 +0530

    created patch request
    .

--- app.js
+++ app.js
@@ -67,6 +67,22 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+   console.log(req.params);
+  if (req.params.id*1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here....>'
+    }
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {

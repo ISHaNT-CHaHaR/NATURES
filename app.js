bff6e944d2102f5f55bf7bf3d00929d6191cea24
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-02-21 13:55:02 +0530

    added tours post
    added particular id for the usee to post tours.

--- app.js
+++ app.js
@@ -24,6 +24,20 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get(`/api/v1/tours/:id`, (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find(el =>  el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+
+    data: {
+      tour
+    }
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   //console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;

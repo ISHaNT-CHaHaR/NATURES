a823a66a7fe84aa7da6f5808c3dc77458e20e23a
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-03-20 22:12:34 +0530

    CRUD operations completed in natours APi

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -118,15 +118,19 @@ exports.createTour = async (req, res) => {
 //   );
 // };
 
-exports.deleteTour = (req, res) => {
-  /////Deletes a single id from json
-  ////////////for deleting a tour.
-  console.log(req.params);
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.updateTour = async (req, res) => {

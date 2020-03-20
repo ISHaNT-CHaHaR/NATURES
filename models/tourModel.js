699957821261badc25a8ac0a4672123686600ec7
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-03-20 23:13:55 +0530

    Tour Model Created
    .

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,16 +5,54 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A Tour must have duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group-size']
+  },
+  difficulty: {
     type: String,
+    required: [true, 'Tour must have a level of difficulty']
+  },
+
+  ratingsAverage: {
+    type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A Tour Must have summary']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A Tour must have acover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date] //Mongo will parse all types of fromats in dates
 });
 ////////////////////////////////SCHEMA ENDED////////////////////////////
 

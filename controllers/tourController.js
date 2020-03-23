0b3753eb521ecb02ba38f17f810feeff57811d22
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-03-24 00:35:11 +0530

    Pagination added,
    .

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -58,12 +58,28 @@ exports.getALLTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    //3.)Limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
     } else {
       query = query.select('-__v');
     }
+
+    //4.)PAgination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) {
+        throw new Error('This page does not exist!');
+      }
+    }
+
     // { duration: { gte: '5' }, difficulty: 'easy' }
     // const tours = await Tour.find()
     //   .where('duration')

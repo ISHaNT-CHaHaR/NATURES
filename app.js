508f4c711563f0820580a43b39fce9e278dc4116
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-03-28 22:23:19 +0530

    global error handling middleware
    Now i need to make a utils class for everything in this middleware and then i will call it everywhre in error handling.(just to remember next thing).

--- app.js
+++ app.js
@@ -39,11 +39,29 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter); /////This is mounting of routers
 
 app.use('/api/v1/users', userRouter);
-
+//////////////////////////////////////////////////////////////////////////////////////
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find  ${req.originalUrl} on this server`
+  //// a router middleware for all other errors.
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find  ${req.originalUrl} on this server`
+  // });
+  const err = new Error(`Can't find  ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err); //// agar next me argument h, then it is automatically an error.
+});
+
+//////////////////////Global HAndler /////////////////////////////
+
+app.use((err, req, res, next) => {
+  //Error function hmesha 4 arguments leta h in express.
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 

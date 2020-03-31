5a86437fdab2f6292347181bbb5f1a82d11811d5
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-03-31 15:37:03 +0530

    completed error handling
    .

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -50,7 +50,7 @@ const handleValidationErrorDB = err => {
   const errors = Object.values(err.errors).map(el => el.message);
 
   const message = `Invalid input Data. ${errors.join('. ')}`;
-  return new AppError(message, 400);
+  return new AppError(message, 400); 
 };
 /////////////////////////////////////////////////////////////////////////////////////
 
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('Uncaught Exception.... Shutting down');
+  console.log(err.message, err.name);
+  process.exit(1);
+});
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -20,6 +25,16 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message); //Safety net.
+  console.log('UNHANDLED REJECION.....Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+//console.log(x);
